7bed1ccc85638e932a43f7cb99b148509e543aca
Trofim <troshakotik@gmail.com>
2025-06-29 14:48:23 +0500

    modified:   .gitignore 	modified:   docker-compose.yml 	modified:   users/admin.py 	new file:   users/forms.py 	new file:   users/migrations/0002_user_phone_number.py 	new file:   users/migrations/0003_user_email.py 	new file:   users/migrations/0004_alter_user_managers_user_is_staff.py 	new file:   users/migrations/0005_alter_user_options_user_is_active.py 	modified:   users/models.py 	modified:   users/tests.py
    create custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .forms import CustomUserChangeForm, CutstomUserCreationForm
 
-# Register your models here.
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CutstomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("username", "email", "phone_number", "is_staff")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,30 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
-from django.db.models import CharField
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.db.models import CharField, EmailField, BooleanField
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import RegexValidator
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = CharField("Имя пользователя", unique=True, validators=[UnicodeUsernameValidator()], max_length=150)
+    phone_validator = RegexValidator(
+        regex=r"^8\d{10}$", message="Допускается только российский номер телефона"
+    )
+    objects = UserManager()
+
+    username = CharField(
+        "Имя пользователя",
+        unique=True,
+        validators=[UnicodeUsernameValidator()],
+        max_length=150,
+    )
+    is_staff = BooleanField("Сотрудник", default=False)
+    is_active = BooleanField("Активен", default=True)
+    phone_number = CharField(
+        "Номер телефона", unique=True, validators=[phone_validator], max_length=20
+    )
+    email = EmailField("Электронная почта", unique=True)
+
     USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ("email", "phone_number")
+
+    class Meta:
+        verbose_name = "Пользователь"
