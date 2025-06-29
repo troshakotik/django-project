e7155a53dd18e8f6bab7f1e213a9a1a6cc80c1db
Trofim <troshakotik@gmail.com>
2025-06-29 20:06:19 +0500

    modified:   pages/tests.py 	modified:   pages/urls.py 	modified:   pages/views.py 	modified:   project/settings.py 	modified:   requirements.txt 	new file:   static/css/base.css 	new file:   static/images/.keep 	new file:   static/js/base.js 	new file:   templates/about.html 	modified:   templates/base.html 	modified:   templates/registration/login.html 	modified:   templates/registration/signup.html add css, js, about page

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -16,3 +16,19 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        self.url = reverse("about")
+        self.response = self.client.get(self.url)
+
+    def test_url_exists_and_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_resolves_aboutpageview(self):
+        view = resolve(self.url)
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
--- project/settings.py
+++ project/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
