5782e48d66cec19c2b95ac5cc28d39f399f36f8b
Trofim <troshakotik@gmail.com>
2025-06-29 18:25:04 +0500

    new file:   pages/__init__.py 	new file:   pages/admin.py 	new file:   pages/apps.py 	new file:   pages/migrations/__init__.py 	new file:   pages/models.py 	new file:   pages/tests.py 	new file:   pages/urls.py 	new file:   pages/views.py 	modified:   project/settings.py 	modified:   project/urls.py 	new file:   templates/home.html Add app "pages" and create home view

---
+++ pages/tests.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse("home"))
+
+    def test_url_exists_and_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     # Local
     "users",
+    "pages",
 ]
 
 MIDDLEWARE = [
